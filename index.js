0733ae708487c4067d57c71bbad5f6fb4d112c21
Samuelwps <samuelrip230@gmail.com>
2022-09-18 17:43:16 -0300

    Releasing access

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const movies = require("./src/movies/movies.json")
 
 
 app.get("/genres", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001")
+    res.setHeader("Access-Control-Allow-Origin", "https://01-catalogo-de-filme.vercel.app")
     return res.json(genres.genres)
 })
 
 app.get("/movies", (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001")
+    res.setHeader("Access-Control-Allow-Origin", "https://01-catalogo-de-filme.vercel.app")
     return res.json(movies.movies)
 })
 
